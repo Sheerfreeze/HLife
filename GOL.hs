015d38c7663f574514601ea7ae05c6a212edc8bc
Brooks J Rady <b.j.rady@gmail.com>
2015-05-27 00:58:16 -0600

    V0.5

--- GOL.hs
+++ GOL.hs
@@ -1,31 +1,29 @@
---Just some imports
-import System.IO
-import Data.Char
-import System.Random
-import Control.Monad
-import System.Environment
-import Control.Concurrent
-import Control.Applicative
-import qualified Data.Map as M
+--Just some imports:
+import System.IO --For reading .txt files
+import Data.Char --For filtering strings
+import System.Random --For generating random staring configurations
+import Control.Monad --For "piping" IO
+import System.Environment --For saving terminal args
+import Control.Applicative --The fancy <$> and <*> functions
+import qualified Data.Map as M --For storing our grid
+
+--Notes:
+--Finish IO
 
 --Type synonyms
 type CoOrd = (Int, Int)
 type Cell  = Char --Either '#' for live or '-' for dead
 type Grid  = M.Map CoOrd Cell
 
---Global varibles
-gS = (30, 30) -- (rows, columns)
+--Global constants
+gS = (40, 100) -- (rows, columns)
 rules = ([2,3],[3]) --(Survival,Birth): Conway's Game of Life
-onChar  = '#'
-offChar = '-'
+onChar  = '#' --"Live" cell
+offChar = '-' --"Dead" cell
 
 --Coordinate tuples
 coords :: [CoOrd]
 coords = (\r c -> (r,c)) <$> [1..(fst gS)] <*> [1..(snd gS)]
-
---Empty grid
-emptyGrid :: Grid
-emptyGrid = M.fromList . zip coords $ repeat offChar
 
 --Converts grid to list of cells
 gridToCells :: Grid -> [Cell]
@@ -37,21 +35,18 @@ cellsToGrid = M.fromList . zip coords . filter isPrint
 
 --Returns all surounding cells of a coordinate
 surCells :: CoOrd -> Grid -> [Cell]
-surCells co g = map (g M.!) (surCo (fst co) (snd co))
-    where inR (r,c) = r `elem` [1..(fst gS)] && c `elem` [1..(snd gS)]
-          rw x = if (x<1) then x+(fst gS) else if (x>(fst gS)) then x-(fst gS) else x
-          cw x = if (x<1) then x+(snd gS) else if (x>(snd gS)) then x-(snd gS) else x
-          surCo r c = filter inR
-                      [(rw (r+0), cw (c+1)), (rw (r+1), cw (c+1))
-                      ,(rw (r+1), cw (c+0)), (rw (r+1), cw (c-1))
-                      ,(rw (r+0), cw (c-1)), (rw (r-1), cw (c-1))
-                      ,(rw (r-1), cw (c+0)), (rw (r-1), cw (c+1))]
+surCells co g = map (g M.!) (surCo co) -- "Looks up" all of the surrounding cells
+    where rw x = if (x<1) then x+(fst gS) else if (x>(fst gS)) then x-(fst gS) else x --Wraps rows
+          cw x = if (x<1) then x+(snd gS) else if (x>(snd gS)) then x-(snd gS) else x --Wraps columns
+          surCo (r,c) = [(rw (r+0), cw (c+1)), (rw (r+1), cw (c+1))
+                        ,(rw (r+1), cw (c+0)), (rw (r+1), cw (c-1))
+                        ,(rw (r+0), cw (c-1)), (rw (r-1), cw (c-1))
+                        ,(rw (r-1), cw (c+0)), (rw (r-1), cw (c+1))]
 
 --Updates grid based on a set of rules
 nextGen :: Grid -> Grid
 nextGen g = cellsToGrid $ next coords g
-    where surOn c g = length . filter (==onChar) $ surCells c g
-          next :: [CoOrd] -> Grid -> String
+    where surOn c g = length . filter (==onChar) $ surCells c g --Returns the number of "live" neighbors
           next [] _     = ""
           next (c:cs) g
               |surOn c g `elem` (snd rules) = [onChar] ++ (next cs g) --Birth
@@ -60,7 +55,7 @@ nextGen g = cellsToGrid $ next coords g
 
 --Prints a list of cells
 printCells :: [Cell] -> IO ()
-printCells [] = putStr ""
+printCells [] = putStrLn ""
 printCells g  = putStrLn (take (snd gS) g) >> printCells (drop (snd gS) g)
 
 --Make flexible command line interface:
@@ -70,12 +65,11 @@ printCells g  = putStrLn (take (snd gS) g) >> printCells (drop (snd gS) g)
 --Set alternitive rules (use -r tag)
 main :: IO()
 main = do
-    args <- getArgs
-    g <- getStdGen
-    if (args == [])
+    args <- getArgs --Stores command line arguments
+    g <- getStdGen --Stores "seed" for the random generator
+    if (args == []) --If no arguements are given then... otherwise...
         then simulate $ cellsToGrid . map (\x -> if x then onChar else offChar) $ randomRs (True,False) g
         else readFile (head args) >>= simulate . cellsToGrid . filter isPrint
-        where simulate g = do
+        where simulate g = do --Prints current grid, updates the grid, then repeats
                         printCells . gridToCells $ g
-                        --threadDelay 100000
                         simulate $ nextGen g
